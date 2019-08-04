230c4e1945a19e4bbb388484509e4224391c82ae
RahulFernando <rahulrulz680@gmail.com>
2019-08-04 21:14:36 +0530

    home page and login

--- URBAN_CREW/app/src/main/java/com/example/urban_crew/MainActivity.java
+++ URBAN_CREW/app/src/main/java/com/example/urban_crew/MainActivity.java
@@ -2,13 +2,25 @@ package com.example.urban_crew;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button started;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        started = (Button)findViewById(R.id.started);
+        started.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent regIntent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(regIntent);
+            }
+        });
     }
 }
